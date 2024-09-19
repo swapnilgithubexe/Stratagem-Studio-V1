6efa12680e56146f2603f0cfd6683eb1d23e668c
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-19 08:30:42 +0530

    updates

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,5 +1,6 @@
 import trycatchfunction from "../middlewares/trycatch.js";
 import { Courses } from "../models/Courses.js"
+import { Lecture } from "../models/lecture.js"
 
 export const createCourse = trycatchfunction(async (req, res) => {
   const { title, description, category, createdBy, duration, price } = req.body;
@@ -14,4 +15,25 @@ export const createCourse = trycatchfunction(async (req, res) => {
   res.status(201).json({
     message: "Course has been created successfully!"
   })
+})
+
+export const addLectures = trycatchfunction(async (req, res) => {
+  const course = await Courses.findById(req.params.id);
+
+  if (!course) {
+    return res.status(404).json({ message: "No course found!" })
+  }
+
+  const { title, description } = req.body;
+
+  const file = req.file;
+
+  const lecture = await Lecture.create({
+    title,
+    description,
+    video: file.path,
+    course: course._id,
+  });
+
+  res.status(201).json({ message: "Lecture Added", lecture })
 })
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { isAdmin, isAuth } from "../middlewares/isAuth.js";
-import { createCourse } from "../controllers/admin.js";
+import { addLectures, createCourse } from "../controllers/admin.js";
 import { uploadFiles } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.post("/course/new", isAuth, isAdmin, uploadFiles, createCourse)
+
+//Lectures
+router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLectures);
 
 export default router;
