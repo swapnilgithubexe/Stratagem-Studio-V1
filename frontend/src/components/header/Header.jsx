a56e051e61cfa5292e762433a4d4a26f788c0dd0
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-19 00:40:38 +0530

    header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">Stratagem Studio</div>
+
+      <div className="link">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/courses"}>Courses</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/account"}>Account</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
